e875b622352ea28a9f159bae1e41f74a9b56d1b1
savio591 <savio591@hotmail.com>
2021-11-11 10:49:24 -0300

    components: logo variations

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,32 +6,44 @@ type Format = 'square' | 'wide'
 interface LogoBaseProps {
   className?: string
   format: Format
+  variation: 'dark' | 'light'
 }
 
 interface LogoProps {
   format: Format
   inABoxDiv?: boolean
+  variation?: 'dark' | 'light'
 }
 
 type LogoBaseElement = ({ format, ...rest }: LogoBaseProps) => ReactElement
 
-const LogoBase: LogoBaseElement = ({ ...rest }) => {
+const LogoBase: LogoBaseElement = ({ format, variation, ...rest }) => {
+  const formatAbvr = format === 'square' ? 's' : 'w'
+  const variationAbvr = variation === 'dark' ? 'd' : 'l'
   return (
-    <Image src="logo-w-c.svg" width={48} height={44} alt="logo" {...rest} />
+    <Image
+      src={`/logo-${formatAbvr}-${variationAbvr}.svg`}
+      width={format === 'square' ? 40: 194}
+      height={37}
+      alt="logo"
+      {...rest}
+    />
   )
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function Logo({ format, inABoxDiv }: LogoProps): JSX.Element {
+export function Logo({ format, inABoxDiv, variation }: LogoProps): JSX.Element {
   if (inABoxDiv) {
     return (
       <div
         className="bg-dark d-flex align-items-center justify-content-center h-100"
-        style={{width: '8rem', maxWidth: '8rem'}}
+        style={{ width: '8rem', maxWidth: '8rem' }}
       >
-        <LogoBase format={format} />
+        <LogoBase variation={variation ?? 'dark'} format={format ?? 'square'} />
       </div>
     )
   }
-  return <LogoBase format={format} />
+  return (
+    <LogoBase variation={variation ?? 'dark'} format={format ?? 'square'} />
+  )
 }
