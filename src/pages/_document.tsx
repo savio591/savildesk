ef9786195bcf9ce7ad66a3f87d1a00e36b2432c6
savio591 <savio591@hotmail.com>
2021-11-11 00:21:08 -0300

    fonts: added Open Sans and Open Sans Condensed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,6 +20,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="https://fonts.googleapis.com"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300;700&family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           {GA_TRACKING_ID && (
             <>
