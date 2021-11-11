4463227a7daf681cc6e9930e4565a975e28391fe
savio591 <savio591@hotmail.com>
2021-11-11 16:47:35 -0300

    Revert "PR #22:  fixes unfocusable header #10  => dev"
    This reverts commit c9d49e855ea4dcc58466ba96691ecccb59da4a45, reversing
    changes made to e342ba8ef5f0f0cdfdd4040041e48e1e55684d58.

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -2,21 +2,16 @@ import { FaSearch } from 'react-icons/fa'
 
 export function SearchBox(): JSX.Element {
   return (
-    <div className="input-group px-5">
+    <div className="input-group">
       <div className="input-group-prepend">
         <span className="input-group-text h-100 bg-transparent border-0 fw-light">
           <FaSearch color="white" />
         </span>
       </div>
-      <label className="visually-hidden-focusable" htmlFor="search">
-        Pesquisar produtos
-      </label>
       <input
         type="text"
         className="form-control border-0 bg-transparent h-100"
         aria-label="search"
-        placeholder="Pesquisar produtos"
-        id="search"
       />
     </div>
   )
